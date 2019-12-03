40abfe9f8017fc4b139108dafacaf4ba5b62d983
Marcus Vinicius Mendes Pereira <56842640+mvmendes-hub@users.noreply.github.com>
2019-12-03 15:49:41 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assigment Coursera's Course - Week 3 - Creating a matrix “dest_a” that can cache its inverse
+## We are assuming an invertible matrix supplied (always)
+## Marcus Vinicius Mendes Pereira; email's address: mvmendes@gmail.com
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(dest_a = matrix()) {
+  mm <- NULL
+  set <- function(source_a){
+    dest_a <<- source_a
+    mm <<- NULL
+  }
+  get <- function()dest_a
+  setInverse <- function(inverse) mm <<- inverse
+  getInverse <- function() mm 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(dest_a, ...) {
+  ## Return a matrix that is the inverse of 'dest_a'
+  mm <- dest_a$getInverse()
+  if(!is.null(mm)){
+    message("getting cached data")
+    return(mm)
+  }
+  mat <- dest_a$get()
+  mm <- solve(mat,...)
+  dest_a$setInverse(mm)
+  mm
 }
